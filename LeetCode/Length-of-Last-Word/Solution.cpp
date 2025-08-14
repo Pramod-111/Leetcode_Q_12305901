b448255ae260f401be73108234fd82ef8e6a28fc
Pramod-111 <nwdpramod12305901@gmail.com>
2025-08-14 20:50:57 +0530

    Add solution for Length of Last Word

--- LeetCode/Length-of-Last-Word/Solution.cpp
+++ LeetCode/Length-of-Last-Word/Solution.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int i=s.size()-1;
-        int length=0;
-        while(s[i]==' '){
+        int i = s.size() - 1;
+        int length = 0;
+        while (s[i] == ' ') {
             i--;
         }
-        while(i>=0 && s[i]!=' '){
+        while (i >= 0 && s[i] != ' ') {
             length++;
             i--;
         }
