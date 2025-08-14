636a8c9910157b92ae447cf2a2360c1986c8a392
Pramod-111 <nwdpramod12305901@gmail.com>
2025-08-14 13:35:34 +0530

    Add solution for Missing Number

--- LeetCode/Missing-Number/Solution.cpp
+++ LeetCode/Missing-Number/Solution.cpp
@@ -1,12 +1,27 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]!=i){
+//                 return i;
+//             }
+//         }
+//         return nums.size();
+//     }
+// };
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=i){
-                return i;
-            }
+        int n = nums.size();
+        int expectedSum = n * (n + 1) / 2; // sum of numbers 0 to n
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
         }
-        return nums.size();
+
+        return expectedSum - actualSum;
     }
 };
