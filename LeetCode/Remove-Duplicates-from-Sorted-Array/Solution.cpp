dd689356d0ca6b0487e034cef8c89d2da4d17d45
Pramod-111 <nwdpramod12305901@gmail.com>
2025-08-14 13:21:07 +0530

    Add solution for Remove Duplicates from Sorted Array

--- LeetCode/Remove-Duplicates-from-Sorted-Array/Solution.cpp
+++ LeetCode/Remove-Duplicates-from-Sorted-Array/Solution.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size()==0){
+        if (nums.size() == 0) {
             return 0;
         }
-        int l=1;
-        for(int r=1;r<nums.size();r++){
-            if(nums[r]!=nums[r-1]){
-                nums[l]=nums[r];
+        int l = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[l] = nums[i];
                 l++;
             }
         }
